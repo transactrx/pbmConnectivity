abd1628f94d987deb5076e2923d41a9fb68a8869
Mauricio Rodriguez <Mauricio.Rodriguez@redsailtechnologies.com>
2024-09-03 11:49:34 -0400

    added urloverride

--- pkg/tlssynch/post.go
+++ pkg/tlssynch/post.go
@@ -16,10 +16,14 @@ func (pc *TLSSyncConnect) Post(claim []byte, header map[string][]string) ([]byte
 	tmp, _ := strconv.Atoi(Cfg.PbmReceiveTimeOut)
 	timeOut := time.Duration(float64(tmp) * float64(time.Second))
 	tid := "Unknown-TID"
+	urlOverride := ""
 	if values, ok := header["transmissionId"]; ok && len(values) > 0 {
 		tid = values[0]
 	}
-	conn, err := Connect(tid)
+	if values, ok := header["urlOverride"]; ok && len(values) > 0 {		
+	   urlOverride = values[0]
+	}
+	conn, err := Connect(tid,urlOverride)
 	if err != pbmlib.ErrorCode.TRX00 {
 
 		log.Printf("tlssynch.Post tid: %s Connect failed, error: '%s'", tid, err.Message)
@@ -35,15 +39,18 @@ func (pc *TLSSyncConnect) Post(claim []byte, header map[string][]string) ([]byte
 	return responseBuffer, nil, pbmlib.ErrorCode.TRX00
 }
 
-func Connect(tid string) (net.Conn, pbmlib.ErrorInfo) {
+func Connect(tid string,urlOverride string) (net.Conn, pbmlib.ErrorInfo) {
 
-	// Combine host and port into an address
+	url := Cfg.PbmUrl
+	if len(urlOverride)>0 {
+		url = urlOverride
+	}
 	address := Cfg.PbmUrl + ":" + Cfg.PbmPort
-	log.Printf("tlssynch.connect tid: %s connecting to '%s' Pbm Certificate Insecure Skip Verify: %t", tid, address,Cfg.PbmInsecureSkipVerify)
+	log.Printf("tlssynch.connect tid: %s connecting to '%s' Pbm Certificate Insecure Skip Verify: %t", tid, address, Cfg.PbmInsecureSkipVerify)
 	// Create a TLS configuration
 	tlsConfig := &tls.Config{
 		InsecureSkipVerify: Cfg.PbmInsecureSkipVerify, // You might want to set this to false in production
-		ServerName: Cfg.PbmUrl,
+		ServerName: url,
 	}
 	// Create a timeout for the connection attempt
 	timeout := 5 * time.Second // Adjust the timeout duration as needed
@@ -74,10 +81,10 @@ func Connect(tid string) (net.Conn, pbmlib.ErrorInfo) {
 func SubmitRequest(claim string, tid string, conn net.Conn, timeout time.Duration) ([]byte, int, pbmlib.ErrorInfo) {
 
 	peerAddr := conn.RemoteAddr().String()
-	
+
 	defer conn.Close()
 
-	log.Printf("tlssynch.submitRequest tid: %s data(16) %.16s time-out value: %f seconds url: %s", tid, claim, timeout.Seconds(),peerAddr)
+	log.Printf("tlssynch.submitRequest tid: %s data(16) %.16s time-out value: %f seconds url: %s", tid, claim, timeout.Seconds(), peerAddr)
 	// Set a read deadline for the connection
 	//conn.SetReadDeadline(time.Now().Add(timeout))
 	// Send a message to the server
@@ -90,18 +97,18 @@ func SubmitRequest(claim string, tid string, conn net.Conn, timeout time.Duratio
 	}
 	// Receive and print the response from the server
 	buffer := make([]byte, PBM_DATA_BUFFER)
-	conn.SetReadDeadline(time.Now().Add(timeout))	
+	conn.SetReadDeadline(time.Now().Add(timeout))
 	bytesRead, err := conn.Read(buffer)
-	if err != nil {		
+	if err != nil {
 		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
 			// Handle the read timeout error
 			log.Printf("tlssynch.submitRequest tid: %s Read conn.Read failed timeout error: %s", tid, err)
 			return nil, 0, pbmlib.ErrorCode.TRX05
-		}		
+		}
 		if bytesRead > 0 { // check this case in case some good data was received
 			log.Printf("tlssynch.submitRequest tid: %s Read.error raised but bytesRead > 0 error: %s bytesRead: %d", tid, err, bytesRead)
 		} else {
-			log.Printf("tlssynch.submitRequest tid: %s Read failed error: %s url: %s", tid, err,peerAddr)
+			log.Printf("tlssynch.submitRequest tid: %s Read failed error: %s url: %s", tid, err, peerAddr)
 			return nil, 0, pbmlib.ErrorCode.TRX10
 		}
 	}
