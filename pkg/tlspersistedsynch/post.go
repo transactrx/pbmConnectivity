e16455f4e3342a8eed774b4d328f23aa89796c50
Mauricio Rodriguez <Mauricio.Rodriguez@redsailtechnologies.com>
2024-10-09 12:10:32 -0400

    if Read errors - disconnect session

--- pkg/tlspersistedsynch/post.go
+++ pkg/tlspersistedsynch/post.go
@@ -21,8 +21,8 @@ func (pc *TLSPersistedSyncConnect) Post(claim []byte, header map[string][]string
 	if values, ok := header["transmissionId"]; ok && len(values) > 0 {
 		tid = values[0]
 	}
-	// MRG 10.5.24 - pass headerCheck boolean & headerCheckValue 
-	
+	// MRG 10.5.24 - pass headerCheck boolean & headerCheckValue
+
 	if values, ok := header["headerValueToCheck"]; ok && len(values) > 0 {
 		requestHeader = values[0]
 	}
@@ -33,17 +33,17 @@ func (pc *TLSPersistedSyncConnect) Post(claim []byte, header map[string][]string
 	// 	headerCheckLen = values[0]
 	// }
 
-	log.Printf("tlspersynch.post tid: %s headerValue: %s FindChnl...", tid,requestHeader)
+	log.Printf("tlspersynch.post tid: %s headerValue: %s FindChnl...", tid, requestHeader)
 
-	session , index, err := Ctx.FindConnection()
+	session, index, err := Ctx.FindConnection()
 	if err != nil {
 		log.Printf("tlspersynch.post tid: %s no channel found", tid)
 		return nil, nil, pbmlib.ErrorCode.TRX10
 	}
-	log.Printf("tlspersynch.post[%d]  tid: %s",index,tid)
+	log.Printf("tlspersynch.post[%d]  tid: %s", index, tid)
 	err = session.Write(index, claim)
 	if err != nil {
-		log.Printf("tlspersynch.post[%d]  tid: %s write failed", index,tid)
+		log.Printf("tlspersynch.post[%d]  tid: %s write failed", index, tid)
 		return nil, nil, pbmlib.ErrorCode.TRX10
 	}
 	//log.Printf("READING.....")
@@ -53,23 +53,21 @@ func (pc *TLSPersistedSyncConnect) Post(claim []byte, header map[string][]string
 
 	//hdrOffset,_ := strconv.Atoi(headerCheckOffset)
 	//hdrLen,_ := strconv.Atoi(headerCheckLen)
-	response, err := session.Read(appCtx, index,requestHeader)
+	response, err := session.Read(appCtx, index, requestHeader)
 	if err != nil {
-		Ctx.IncrementError(index)
 		if err == context.DeadlineExceeded {
-			log.Printf("tlspersynch.post[%d]  tid: %s read failed error: timeout disconnecting",index, tid)
+			log.Printf("tlspersynch.post[%d]  tid: %s read failed error: timeout disconnecting", index, tid)
 			Ctx.ReleaseConnection(index)
-			Ctx.DisconnectSession(index)			 
+			Ctx.DisconnectSession(index)
 			return nil, nil, pbmlib.ErrorCode.TRX05
 		} else {
-			log.Printf("tlspersynch.post[%d]  tid: %s read failed error: %v",index,tid ,err)
+			log.Printf("tlspersynch.post[%d]  tid: %s read failed error: %v", index, tid, err)
 			Ctx.ReleaseConnection(index)
+			Ctx.DisconnectSession(index)
 			return nil, nil, pbmlib.ErrorCode.TRX10
 		}
 
 	}
-	Ctx.ClearError(index)
-	Ctx.ReleaseConnection(index)
 
 	//log.Printf("Response: %s", string(response))
 
