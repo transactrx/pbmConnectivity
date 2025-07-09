cfcd815479b27af29a4c62094dfa892740326e6a
Christine Barker <Christine.Savvakis@gmail.com>
2025-07-09 15:11:29 -0400

    tlssynch - logging readability

--- pkg/tlssynch/sitehealth.go
+++ pkg/tlssynch/sitehealth.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"math"
 	"time"
+	"fmt"
 )
 
 func EvaluateSiteHealth() {
@@ -125,7 +126,7 @@ func StartSiteResetMonitor() {
 			now := time.Now()
 			for i := range Sites {
 				site := &Sites[i]
-				log.Printf("Site[%d]: %v", i, site)
+				log.Printf("%s", site.PrintStats())
 
 				if !site.Paused {
 					site.failedClaims.Store(0)
@@ -148,4 +149,27 @@ func StartSiteResetMonitor() {
 			}
 		}
 	}()
+}
+
+func (s *Site) PrintStats() string {
+	line := ""
+	if s == nil {
+		return ""
+	}
+
+	activeInt := 0
+	pauseInt := 0
+	if s.Active {
+		activeInt = 1
+	}
+	if s.Paused {
+		pauseInt = 1
+	}
+	if !s.lastPausedTime.IsZero() {
+		line = fmt.Sprintf("pbmsitestats url: %s active: %d inprocess: %d failed: %d paused: %d pausecount: %d failurerate: %f lastpaused: %s", s.URL, activeInt, s.activeClaims.Load(), s.failedClaims.Load(), pauseInt, s.pauseCount, s.failureRate, s.lastPausedTime)
+	} else {
+		line = fmt.Sprintf("pbmsitestats url: %s active: %d inprocess: %d failed: %d paused: %d pausecount: %d failurerate: %f lastpaused: never", s.URL, activeInt, s.activeClaims.Load(), s.failedClaims.Load(), pauseInt, s.pauseCount, s.failureRate)
+	}
+
+	return line
 }
